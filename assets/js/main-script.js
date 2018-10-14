0927ac03ac101eeb738d0215a61d17796f6d0ec2
coco <coco@lacueva.tv>
2018-10-14 11:02:33 -0300

    le agregue la funcion para iniciar animaciones, pero todavía estan desactivadas

--- assets/js/main-script.js
+++ assets/js/main-script.js
@@ -60,7 +60,8 @@
 
         //inicial los parallax
         initParallax();
-        
+
+
     });//window load
     
 
@@ -222,5 +223,46 @@
     }//initParallax()
 
     
+    /*
+    * IN VIEW ANIMATION
+    * loop se usa como true o false, si es true, la animacion se ejecuta siempre, es decir el elemento entra y sale del view y cada vez se ejecuta la animación, en cambio en false solo se ejecuta una vez y luego queda fija
+    */
+    function startAnimations( clase, loop ) {
+        var $animation_elements = $(clase);
+        var $window = $(window);
+
+        function check_if_in_view() {
+            var window_height = $window.height();
+            var window_top_position = $window.scrollTop();
+            var window_bottom_position = (window_top_position + window_height);
+
+            $.each($animation_elements, function() {
+                var $element = $(this);
+                var element_height = $element.outerHeight();
+                var element_top_position = $element.offset().top;
+                var element_bottom_position = (element_top_position + element_height);
+
+                if ( loop ) {
+                    //check to see if this current container is within viewport
+                    if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+                    $element.addClass('in-view');
+                    } else {
+                        $element.removeClass('in-view');
+                    }
+                } else {
+                    //check to see if this current container is within viewport
+                    if ((element_bottom_position >= window_top_position) && (element_top_position <= window_bottom_position)) {
+                        $element.addClass('in-view');
+                    }
+                }
+                
+            });
+
+        }//check_if_in_view
+
+        $window.on('scroll resize', check_if_in_view);
+        $window.trigger('scroll');
+    }//startAnimations()
+
 
 } )( jQuery );
