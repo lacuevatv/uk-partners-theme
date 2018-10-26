e2fdbd7b0ce596fcb2cf56df438a13dae4356596
coco <coco@lacueva.tv>
2018-10-26 14:37:32 -0300

    ajustes page about

--- page-about.php
+++ page-about.php
@@ -18,148 +18,150 @@ if ( have_posts() ) :  the_post() ?>
 
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header page-header">
-        <div class="header-image">
-            <?php 
-            if ( has_post_thumbnail()) {
-                the_post_thumbnail(); 
-                echo '<span class="shutter"></span>';
-            }
-            ?>  
-		</div>
-		
-        <div class="wrap">
-            <?php the_title( '<h1 class="entry-title page-title-header">', '</h1>' ); ?>
-        </div>
-        
-	</header><!-- .entry-header -->
-	<div class="entry-content page-content-wrapper">
-        <div class="wrap">
-            <div class="main-content-editor">
-			
-				<?php the_content(); ?>
-
-            </div>
-		</div>
-		
-		<section class="crew-section">
-			<div class="wrap">
-			
-			<!--destacados -->
-				<?php
-				$crewDestacado = new WP_Query( array(
-						'post_type' => 'crew',
-						'tax_query' => array(
-							array(
-								'taxonomy' => 'catcrew',
-								'field'    => 'category_name',
-								'terms'    => 'destacado',
-								),
-							),
-						)
-					);
-					
-				if ( $crewDestacado->have_posts() ) : ?>
-		
-                <ul class="crew-list jefes">
-				
-					<?php while ( $crewDestacado->have_posts() ) : $crewDestacado->the_post();
-					$nombre = get_the_title();
-					$parrafo = get_the_content();
-					$cargo = 'Cargo';
+		<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header page-header">
+				<div class="header-image">
+					<?php 
 					if ( has_post_thumbnail()) {
-						$imagen = get_the_post_thumbnail(); 
-					} else {
-						$imagen = IMAGES_DIR . 'default_persona.png';
+						the_post_thumbnail(); 
+						echo '<span class="shutter"></span>';
 					}
-
-					?>
+					?>  
+				</div>
+				
+				<div class="wrap">
+					<?php the_title( '<h1 class="entry-title page-title-header">', '</h1>' ); ?>
+				</div>
+				
+			</header><!-- .entry-header -->
+			<div class="entry-content page-content-wrapper">
+				<div class="wrap">
+					<div class="main-content-editor">
 					
-					<li>
-						<article class="crew-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<div class="image-crew load-images-ajax">
-									<img data-src="<?php echo $imagen; ?>" alt="Sobre Uk Partners">
-								</picture>
-							</div>
-							<h1>
-								<?php echo $nombre; ?>
-							</h1>
-							<h2>
-								<?php echo $cargo; ?>
-							</h2>
-							
-							<?php echo $parrafo; ?>
-							
-						</article>
-					</li>
-				
-					<?php endwhile; ?>
-				
-				</ul>
+						<?php the_content(); ?>
 
-				<?php endif; ?>
-				<?php wp_reset_postdata(); ?>
-
-				<!--otros -->
-				<?php
-					$crew = new WP_Query( array(
-							'post_type' => 'crew',
-							'tax_query' => array(
-								array(
-									'taxonomy' => 'catcrew',
-									'field'    => 'category_name',
-									'operator' => 'NOT IN',
-									'terms'    => 'destacado',
+					</div>
+				</div>
+				
+				<section class="crew-section">
+					<div class="wrap">
+					
+					<!--destacados -->
+						<?php
+						$crewDestacado = new WP_Query( array(
+								'post_type' => 'crew',
+								'tax_query' => array(
+									array(
+										'taxonomy' => 'catcrew',
+										'field'    => 'category_name',
+										'terms'    => 'destacado',
+										),
 									),
-								),
-							)
-						);
-						
-				if ( $crew->have_posts() ) : ?>
-
-				<ul class="crew-list">
-				
-					<?php while ( $crew->have_posts() ) :$crew->the_post();
-					$nombre = get_the_title();
-					$parrafo = get_the_content();
-					$cargo = 'Cargo';
-					if ( has_post_thumbnail()) {
-						$imagen = get_the_post_thumbnail(); 
-					} else {
-						$imagen = IMAGES_DIR . 'default_persona.png';
-					}
-					?>
-						
-					<li>
-						<article class="crew-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-							<div class="image-crew load-images-ajax">
-									<img data-src="<?php echo $imagen; ?>" alt="Sobre Uk Partners">
-								</picture>
-							</div>
-							<h1>
-								<?php echo $nombre; ?>
-							</h1>
-							<h2>
-								<?php echo $cargo; ?>
-							</h2>
+								)
+							);
 							
-							<?php echo $parrafo; ?>
+						if ( $crewDestacado->have_posts() ) : ?>
+				
+						<ul class="crew-list jefes">
+						
+							<?php while ( $crewDestacado->have_posts() ) : $crewDestacado->the_post();
+							$nombre = get_the_title();
+							$parrafo = get_the_content();
+							$cargo = 'Cargo';
+							if ( has_post_thumbnail()) {
+								$imagen = get_the_post_thumbnail(); 
+							} else {
+								$imagen = IMAGES_DIR . 'default_persona.png';
+							}
+
+							?>
 							
-						</article>
-					</li>
-				
-					<?php endwhile; ?>
-				
-				</ul>
+							<li>
+								<article class="crew-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<div class="image-crew load-images-ajax">
+											<img data-src="<?php echo $imagen; ?>" alt="Sobre Uk Partners">
+										</picture>
+									</div>
+									<h1>
+										<?php echo $nombre; ?>
+									</h1>
+									<h2>
+										<?php echo $cargo; ?>
+									</h2>
+									
+									<?php echo $parrafo; ?>
+									
+								</article>
+							</li>
+						
+							<?php endwhile; ?>
+						
+						</ul>
 
-				<?php endif; ?>
-			
-			</div>
+						<?php endif; ?>
+						<?php wp_reset_postdata(); ?>
 
-		</section>
-	</div><!-- .entry-content -->
-</article><!-- #post-## -->
+						<!--otros -->
+						<?php
+							$crew = new WP_Query( array(
+									'post_type' => 'crew',
+									'tax_query' => array(
+										array(
+											'taxonomy' => 'catcrew',
+											'field'    => 'category_name',
+											'operator' => 'NOT IN',
+											'terms'    => 'destacado',
+											),
+										),
+									)
+								);
+								
+						if ( $crew->have_posts() ) : ?>
+
+						<ul class="crew-list">
+						
+							<?php while ( $crew->have_posts() ) :$crew->the_post();
+							$nombre = get_the_title();
+							$parrafo = get_the_content();
+							$cargo = 'Cargo';
+							if ( has_post_thumbnail()) {
+								$imagen = get_the_post_thumbnail(); 
+							} else {
+								$imagen = IMAGES_DIR . 'default_persona.png';
+							}
+							?>
+								
+							<li>
+								<article class="crew-item" id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+									<div class="image-crew load-images-ajax">
+											<img data-src="<?php echo $imagen; ?>" alt="Sobre Uk Partners">
+										</picture>
+									</div>
+									<h1>
+										<?php echo $nombre; ?>
+									</h1>
+									<h2>
+										<?php echo $cargo; ?>
+									</h2>
+									
+									<?php echo $parrafo; ?>
+									
+								</article>
+							</li>
+						
+							<?php endwhile; ?>
+						
+						</ul>
+
+						<?php endif; ?>
+					
+					</div>
+
+				</section>
+			</div><!-- .entry-content -->
+		</div><!-- #post-## -->
+	</div>
+</main>
 
 <?php endif;
 get_footer();
